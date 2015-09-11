b01b6a86e3c8318f4ef360f551a4710ad79335f1
Mitchell Arnett <marnett@umiacs.umd.edu>
2015-09-11 11:49:48 -0400

    get results from Next page in query

--- solidity/solidity/spiders/github_spider.py
+++ solidity/solidity/spiders/github_spider.py
@@ -8,8 +8,20 @@ class SoliditySpider(scrapy.Spider):
         "https://github.com/search?utf8=%E2%9C%93&q=solidity",
     ]
 
+    #def parase(self, response):
+
+
+
+        #yield scrapy.Request(url, callback = self.parse_page)
+
     def parse(self, response):
         for sel in response.xpath('//ul/li/h3'):
             item = SolidityItems()
             item['link'] = sel.xpath('a/@href').extract()
             yield item
+
+        for sel in response.xpath('//div[@class="pagination"]'):
+            item = SolidityItems()
+            url = sel.xpath('a[contains(text(), "Next")]/@href').extract()
+            print url
+            print response
