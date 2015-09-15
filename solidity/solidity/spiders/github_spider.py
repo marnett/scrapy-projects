54c06f3511d970548fb2814f1dcd6c0a519072a7
Mitchell Arnett <marnett@umiacs.umd.edu>
2015-09-14 23:31:53 -0400

    added parse_repo which parses every item in a repo for its link -- still need to differentiate between a repo dir and a repo file

--- solidity/solidity/spiders/github_spider.py
+++ solidity/solidity/spiders/github_spider.py
@@ -17,12 +17,25 @@ class SoliditySpider(scrapy.Spider):
     def parse(self, response):
         for sel in response.xpath('//ul/li/h3'):
             item = SolidityItems()
-            item['link'] = sel.xpath('a/@href').extract()
-            yield item
-
+            link = sel.xpath('a/@href').extract()
+            item['link'] = link
+            #yield item
+            link = "".join(link)
+            link = "https://github.com" + link
+            yield scrapy.Request(link, callback=self.parse_repo)
+ 
         item = SolidityItems()
         url = response.xpath('//div[@class="pagination"]').xpath('a[contains(text(), "Next")]/@href').extract()
         #print "THIS IS THE URL "  + str(url[0])
         #print "THIS IS THE RESPONSE " + str(response)
         url = "https://github.com" + str(url[0])
         yield scrapy.Request(url, callback=self.parse)
+
+
+    def parse_repo(self, response):
+        print "THIS IS THE RESPOSNE" + str(response)
+        for sel in response.xpath('//table/tbody/tr/td[@class="content"]/span'):
+            item = SolidityItems()
+            link = sel.xpath('a/@href').extract()
+            item['link'] = link
+            yield item
